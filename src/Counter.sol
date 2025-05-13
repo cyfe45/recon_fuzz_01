9111a3db9ba16408f15075923a6665aa92308c5f
gjaerk <kgjaeringen@gmail.com>
2025-05-13 07:57:38 +0100

    Initial commit

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,13 +2,26 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
+    uint256 public number = 1;
 
+    /// @notice sets the value of the number variable
+    /// @notice number can never be set to 0
+    /// @param newNumber the value to set number to
     function setNumber(uint256 newNumber) public {
-        number = newNumber;
+        if (newNumber != 0) {
+            number = newNumber;
+        }
     }
 
+    /// @notice increments the existing number value
     function increment() public {
         number++;
+        _incrementHelper();
+    }
+
+    function _incrementHelper() internal {
+        if(number == 5) {
+            number = 0;
+        }
     }
 }
